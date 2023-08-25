ccc8061c374678ebc89c4a2a384547dd261d98f6
abir_26 <78580740+AbirChoudhury-26@users.noreply.github.com>
2023-08-25 18:15:28 +0530

    Add files via upload
    Added files for Exception handling in Asp.net

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FirstCoreWebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var app = builder.Build();
+
+            //If the Environment is Development, Please Show the Unhandled Exception Details 
+            if (app.Environment.IsDevelopment())
+            {
+                //Using UseDeveloperExceptionPage Middleware to Show Exception Details
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.MapGet("/", async context =>
+            {
+                int Number1 = 10, Number2 = 0;
+                int Result = Number1 / Number2; //This statement will throw Runtime Exception
+                await context.Response.WriteAsync($"Result : {Result}");
+            });
+
+            //This will Start the Application
+            app.Run();
+        }
+    }
+}
