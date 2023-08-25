42adae3d3b5ae8d40c2e3b2ab87a25affbcf548d
abir_26 <78580740+AbirChoudhury-26@users.noreply.github.com>
2023-08-25 18:06:03 +0530

    Add files via upload
    Added al the files for Second Core Web Application for asp.net core practice

---
+++ SecondCoreWebApplication/AppSettingJson/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FirstCoreWebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            // Get the Configuration Service.
+            ConfigurationManager configuration = builder.Configuration;
+
+            //Get the Value
+            string MyCustomKeyValue = configuration.GetValue<string>("MyCustomKey");
+            //string MyCustomKeyValue = configuration["MyCustomKey"];
+
+            app.MapGet("/", () => $"{MyCustomKeyValue}");
+
+            app.Run();
+        }
+    }
+}
---
+++ SecondCoreWebApplication/SecondCoreWebApplication/Program.cs
@@ -0,0 +1,14 @@
+namespace SecondCoreWebApplication;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Worker Process Name : " + System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+
+        app.Run();
+    }
+}
