d47e956f07ac078280359b15f2f8f421b69830b9
abir_26 <78580740+AbirChoudhury-26@users.noreply.github.com>
2023-08-25 18:03:36 +0530

    Add files via upload
    Added al the files for First Core Web Application for asp.net core practice

---
+++ FirstCoreWebApplication/FirstCoreWebApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FirstCoreWebApplication;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello Abir,Welcome to the world of TECH");
+
+        app.Run();
+    }
+}
+
