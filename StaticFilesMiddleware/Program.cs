32ae5e437047558b0af2b5a1bd8f9122b1176104
abir_26 <78580740+AbirChoudhury-26@users.noreply.github.com>
2023-08-25 18:16:44 +0530

    Add files via upload
    Added all the files for Static files in asp.net

---
+++ StaticFilesMiddleware/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StaticFilesMiddleware;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.UseStaticFiles();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
+
